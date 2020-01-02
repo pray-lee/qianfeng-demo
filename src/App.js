2dee445baa731a7e72093e90938ba8997940b32b
pray-lee <praycis@gmail.com>
2020-01-02 21:22:25 +0800

    init react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// react的样式
+// classnames npm
+import classNames from 'classnames'
+// styled-components
+import styled from 'styled-components'
 
 function App() {
+  // 行内样式
+  const style={
+    color:'#f00'
+  }
+  // styled-components
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+    .detail{
+      color:#ff5252
+    }
+  `
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header" style={style}>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,8 +35,57 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <p className={classNames('a', {'b': true}, {'c': false})}>
+          使用classnames npm 包管理class
+        </p>
+      </main>
+      <footer>
+        <Title>使用styled-components管理样式
+          <p class="detail">支持和className一起混用</p>
+        </Title>
+      </footer>
     </div>
   );
 }
+
+// jsx原理
+// class的jsx渲染编译成下面这样
+
+// const appVDom = {
+//   tag: 'div',
+//   attributes: {
+//     className: 'App'
+//   },
+//   children: [
+//     {
+//       tag: 'p',
+//          attributes: null,
+//       children: [
+//         {
+//           tag: 'code'
+//         }
+//       ]
+//     }
+//   ]
+// }
+
+// render() {
+//   return (
+//     React.createElement(
+//       'div',
+//       {
+//         className: 'App'
+//       },
+//       React.createElement(
+//         'p',
+//         null,
+//         'Edit and reload it'
+//       )
+//     )
+//   )
+// }
+
+
 
 export default App;
